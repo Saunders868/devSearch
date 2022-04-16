74f70479ee9c69b719b4b709bb20f6a0cbb8d848
Daniel Simeon Sedeq Saunders <saundersdaniel.10@gmail.com>
2022-04-15 22:36:13 -0400

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,43 @@
+
+
+let searchForm = document.getElementById('searchForm')
+let pageLinks = document.getElementsByClassName('page-link')
+
+if (searchForm) {
+    for (let i = 0; pageLinks.length > i; i++) {
+        pageLinks[i].addEventListener('click', function (e) {
+            e.preventDefault()
+
+            // get data attributes
+            let page = this.dataset.page
+
+            // add hidden search input to form
+            searchForm.innerHTML += `<input value=${page} name="page" hidden />`
+
+            // submit form
+            searchForm.submit()
+        })
+    }
+}
+
+let tags = document.getElementsByClassName("project-tag")
+// console.log(tags)
+
+for (let i = 0; tags.length > i; i++) {
+    tags[i].addEventListener('click', (e) => {
+        let tagId = e.target.dataset.tag
+        let projectId = e.target.dataset.project
+
+        // console.log(tagId,projectId)
+
+        fetch('http://localhost:8000/api/remove-tag/', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 'project': projectId, 'tag': tagId })
+        }).then(response => response.json()).then(data => {
+            e.target.remove()
+        })
+    })
+}
